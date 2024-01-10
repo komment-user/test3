03e61a67f917518ec18342241bc4e908f8befd23
Nathan Bateman <150971763+komment-user@users.noreply.github.com>
2024-01-10 18:52:21 +0100

    Create code.ts

---
+++ code.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Inject } from "@angular/core";
+import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material";
+
+export interface DialogData {
+  name: string;
+  login: string;
+}
+
+@Component({
+  selector: "app-no-scatter",
+  templateUrl: "./no-scatter.component.html",
+  styleUrls: ["./no-scatter.component.scss"],
+})
+export class NoScatterComponent implements OnInit {
+  constructor(
+    public dialogRef: MatDialogRef<NoScatterComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData
+  ) {}
+
+  ngOnInit() {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+}
